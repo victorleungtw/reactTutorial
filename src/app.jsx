55a7ba1b26e9ed35dd62498b560f0b5b60d341cd
Victor Leung <victorleungtw@gmail.com>
2014-11-23 11:13:36 +0800

    worked till step 5

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,38 @@
-'use strict';
-
 var React = require('react');
+
+var boxStyle = {
+  height: '100px',
+  width: '100px'
+};
+
+var Box = React.createClass({
+  getInitialState: function(){
+    return {
+      value: 'X'
+    }
+  },
+
+  componentWillMount: function(){
+    this.timer = setInterval(function(){
+      var value = this.state.value;
+      value = value === "X" ? "O": "X";
+      this.setState({
+        value: value
+      });
+    }.bind(this), 300);
+  },
+
+  componentWillUnmount: function(){
+    clearInterval(this.timer);
+  },
+
+  render: function(){
+    return (
+      <button style={boxStyle}>
+        {this.state.value}
+      </button>
+    );
+  }
+});
+
+React.render(<Box/>, document.body);
