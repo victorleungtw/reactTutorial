062bc6b937d4aa978e005f8be6f3f4a0cd3475db
Victor Leung <victorleungtw@gmail.com>
2014-11-23 11:48:05 +0800

    proceed to step 9

--- src/app.jsx
+++ src/app.jsx
@@ -6,9 +6,13 @@ var boxStyle = {
 };
 
 var Box = React.createClass({
+  handleClick: function(){
+    this.props.handleClick(this.props.index);
+  },
   render: function(){
     return (
       <button
+        onClick={this.handleClick}
         style={boxStyle}>
         {this.props.value}
       </button>
@@ -23,12 +27,26 @@ var Row = React.createClass({
       clicks: 0
     }
   },
+  handleClick: function(index){
+    var boxValues = this.state.boxValues;
+    var clicks = this.state.clicks;
+    boxValues[index] = clicks % 2 ? 'X' : 'O';
+    this.setState({
+      clicks: clicks + 1,
+      boxValues: boxValues
+    });
+  },
   render: function(){
     var cBoxes = this.state.boxValues.map(function(value, index){
       return (
-        <Box value={value} key={index} index={index}/>
+        <Box
+          handleClick={this.handleClick}
+          value={value}
+          key={index}
+          index={index}
+        />
       );
-    });
+    }.bind(this));
     return (
       <div>
         {cBoxes}
